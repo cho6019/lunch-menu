7f556d93d7470eccaf90541483286951af44a40c
cho6019 <j01040864205@gmail.com>
2025-02-11 11:11:03 +0900

    망할api f

--- pages/5_Api.py
+++ pages/5_Api.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import datetime
 
 
 st.set_page_config(page_title="API", page_icon="🍽️")
@@ -7,12 +8,20 @@ st.set_page_config(page_title="API", page_icon="🍽️")
 st.markdown("# 🍽️ API")
 st.sidebar.header("나이계산기")
 
-dt = st.date_input('생일입력')
+dt = st.date_input('생일입력', min_value=datetime.date(1900,1,1))
 if st.button("나이계산"):
-    response = requests.get('https://ac.sunsin.shop/api/py/ageCalculator/{dt}')
-    result = response.json()
-    st.write(f"당신의 나이는 {result['age']}세!")
+    headers={
+        'accept': 'application/json'
+    }
+    response = requests.get(f'https://agecalculator.cho6019.store/api/py/ageCalculator/birthday/{dt}', headers=headers)
+    st.write(response.status_code)
     
-    st.success(f'나이계산{dt}')
+    if response.status_code == 200:
+        result = response.json()
+        age = result["age"]
+        st.write(f'당신의 나이는 {age}세!')
+        st.success(f'나이계산{dt}')
+    else:
+        st.write(f'what?{response.status_code}')
     
     
